c2eac13b6b0b4756fc451e43e7e9674f6751ae19
madhavi638 <158804547+madhavi638@users.noreply.github.com>
2026-06-29 10:12:43 +0530

    Create pytest_script/utils/config.py

---
+++ pytest_script/utils/config.py
@@ -0,0 +1,8 @@
+# utils/config.py
+
+BASE_URL = "https://www.saucedemo.com"
+BROWSER = "chrome"
+IMPLICIT_WAIT = 10
+EXPLICIT_WAIT = 15
+SCREENSHOT_DIR = "reports/screenshots"
+REPORT_DIR = "reports"
